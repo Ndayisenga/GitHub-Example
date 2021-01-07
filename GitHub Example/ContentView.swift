5367baccd86c98879d06cee98497fe2d1f891980
John <cloude056@yahoo.fr>
2021-01-07 12:37:52 +0200

    modified

--- GitHub Example/ContentView.swift
+++ GitHub Example/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        Spacer()
+        
         Text("Hello, world!")
             .padding()
+        Text("Hello , Mr John ").padding().background(Color.blue)
+        
+        Spacer()
+        
+        Label(/*@START_MENU_TOKEN@*/"Label"/*@END_MENU_TOKEN@*/, systemImage: /*@START_MENU_TOKEN@*/"42.circle"/*@END_MENU_TOKEN@*/)
+        
+        Spacer()
+        
+        Text("write something here ")
+        
+        Spacer()
+        
+        Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+           
+            
+        }
     }
 }
 
